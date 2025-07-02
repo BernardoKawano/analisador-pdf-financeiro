e9a1f28828f47b384d4ae55cca8ff3940b6e5073
Bernardo Dias Machado Kawano <kawano.ber@gmail.comcom>
2025-07-02 01:52:12 -0300

    Adiciona __init__.py para definir o pacote app

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,8 @@
 Módulo src - Analisador de PDF
 """
 
+# __init__.py
+# Define o pacote app para a aplicação Analisador PDF Financeiro.
+
 __version__ = "1.0.0"
 __author__ = "Bernardo Dias Machado Kawano"
